79d7dcc99e6a631591929b273fa71762a7c655ac
SaniyaNaeem123 <168041350+SaniyaNaeem123@users.noreply.github.com>
2025-08-18 02:40:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var modules_js_1 = require("./modules.js");
+console.log(modules_js_1.fullName);
---
+++ modules.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fullName = fullName;
+function fullName() {
+    var name1 = "Taha";
+    var name2 = "Naeem";
+    return name1 + name2;
+}
+console.log(fullName());
